6d873fe0bd60cddf22d41ad5a7a3ea74f635843b
Pyotr Ryzhov <maestro_pete@inbox.ru>
2022-03-22 12:07:38 +0300

    styled main-menu mobile

--- src/js/script.js
+++ src/js/script.js
@@ -7,5 +7,22 @@ function ready(fn) {
 }
 
 ready(function(){
-  console.log('DOM ready');
+  // MAIN MENU
+  const BurgerBtn = document.querySelector('#main-nav-burger');
+  const MainNav = document.querySelector('#main-nav');
+  const MainNavLinks = document.querySelectorAll('.main-nav__link');
+
+  if (BurgerBtn && MainNav && MainNavLinks) {
+
+    const toggleMainMenu = () => {
+      BurgerBtn.classList.toggle('active');
+      MainNav.classList.toggle('active');
+    }
+
+    BurgerBtn.addEventListener('click', (ev) => toggleMainMenu());
+
+    MainNavLinks.forEach(item => {
+      item.addEventListener('click', () => toggleMainMenu())
+    });
+  }
 });
